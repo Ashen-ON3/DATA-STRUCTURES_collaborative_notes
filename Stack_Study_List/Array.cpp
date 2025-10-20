4a78c916604eede755b21b894e3a49b13f281e85
Jaffe Jairus Yongco <kzzfell536@gmail.com>
2025-10-20 22:43:46 +0800

    Create Array.cpp

---
+++ Stack_Study_List/Array.cpp
@@ -0,0 +1,57 @@
+#include "Array.h"
+
+Array::Array(int capacity) {
+    this->capacity = capacity;
+    size = 0;
+    data = new int[capacity];
+}
+
+Array::~Array() {
+    delete[] data;
+}
+
+void Array::insert(int value) {
+    if(size == capacity){
+        cout << "Array is full. Cannot insert.";
+    }
+    else{
+    data[size] = value; 
+    size++;
+    }
+}
+
+void Array::removeAt(int index) {
+    int temp = 0;
+    if((index >= 0 )&&(index <= size)){
+         for(int i = index; i < size;i++){
+        temp = data[i];
+        data[i] = data[i + 1];
+        data[i + 1] = temp;
+      }
+      size--;
+    }
+
+}
+
+int Array::get(int index) const {
+    if(index > size || index < 0){
+        cout << "Invalid index.";
+        return -1; 
+    }
+    else{
+        return data[index];
+    }
+}
+
+int Array::getSize() const {
+    return size;
+}
+
+void Array::print() const {
+    cout << "[ ";
+    for (int i = 0; i < size; i++) {
+        cout << data[i];
+        if (i < size - 1) cout << ", ";
+    }
+    cout << " ] Size: " << size << " / Capacity: " << capacity << "\n";
+}
